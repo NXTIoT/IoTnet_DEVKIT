2889f6aa7084412c7a6c2cee8412ad29c7638fe1
NXTIoT <33075322+NXTIoT@users.noreply.github.com>
2017-11-09 18:23:30 -0600

    Update sensor_temperatura.ino

--- Ejemplos/sensor_temperatura/sensor_temperatura.ino
+++ Ejemplos/sensor_temperatura/sensor_temperatura.ino
@@ -1,31 +1,43 @@
-#define trigPin 3
-#define echoPin 2
+#define SigfoxDeb
+#include <SoftwareSerial.h>
+#ifdef SigfoxDeb
+SoftwareSerial mySerial(0, 1); // RX, TX
+#endif
+
+const int sensorPin = A5;
+const float baselinetime=20.0;
 const int boton=6;
+char RespuestaSigfox[50];
+char ID[51];
+char PAC[51];
 
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  #ifdef SigfoxDeb
+  mySerial.begin(9600);
+  #endif
+  //pinMode(4, OUTPUT);
   pinMode(boton, INPUT);
   pinMode(7, OUTPUT);
+  #ifdef SigfoxDeb
+  mySerial.println("\n\n\n\n\n\n\n\n\rInicio");
+  #endif
 }
 
-void leer_distancia()
+void leer_sensor()
 {
   String bufer="AT$SF=";
-  String bufer2="\n";
-  float duracion, distancia ;
-  digitalWrite(trigPin, LOW);        
-  delayMicroseconds(2);              
-  digitalWrite(trigPin, HIGH);       
-  delayMicroseconds(10);            
-  digitalWrite(trigPin, LOW);       
-  duracion = pulseIn(echoPin, HIGH) ;
-  distancia = duracion / 2 / 29.1;
-  Serial.print("Distancia: ") ;
-  Serial.println(distancia) ;
-  byte* a1 = (byte*) &distancia;
+  char payload[20];
+  int sensorVal=analogRead(sensorPin);
+  float voltaje=(sensorVal/1024.0)*5;
+  Serial.print("Voltaje: ");
+  Serial.println(voltaje); 
+  Serial.print("Grados Cº: ");
+  float temp=((voltaje)*100)+1;
+  Serial.println(temp);
+  //convierte el dato a bytes y lo agrega a nuestro mensaje a enviar
+  byte* a1 = (byte*) &temp;  
   String str1;
   for(int i=0;i<4;i++)
   {
@@ -38,23 +50,47 @@ void leer_distancia()
     {
       bufer+=str1;
     }
-  }  
-  bufer+=bufer2;
+  }
+  bufer.toCharArray(payload,16);
   digitalWrite(7, HIGH);
   delay(1000);
-  Serial.print("AT$RC\n");
-  Serial.print(bufer);
-  delay(3000);
+  enviarcomandoATSigfox("AT");
+  enviarcomandoATSigfox("AT$RC");
+  enviarcomandoATSigfox(payload);
   digitalWrite(7, LOW);
-  delay(500);
 }
 
 void loop() 
 {
   if (digitalRead(boton)==LOW)
-  { 
-    leer_distancia();
-    delay(1000);
-  } 
+  {
+    leer_sensor();
+    delay(2000);
+  }
 }
 
+void enviarcomandoATSigfox(char* comandoAT){
+  unsigned long x=0;
+  #ifdef SigfoxDeb
+  mySerial.print("\r\n\tSigfox-->");
+  mySerial.println(comandoAT);
+  #endif
+  while( Serial.available() > 0) Serial.read();
+  x = 0;
+  memset(RespuestaSigfox, '\0',sizeof(RespuestaSigfox)); 
+  Serial.print(comandoAT);
+  Serial.print("\r\n");
+  while(true){
+    if(Serial.available() != 0){   
+      RespuestaSigfox[x] = Serial.read();
+      x++;
+      if (strstr(RespuestaSigfox, "\n") != NULL){
+        #ifdef SigfoxDeb
+        mySerial.print("Comando OK\n\r");
+        mySerial.println(RespuestaSigfox);
+        #endif
+        break;
+      }
+    }
+  }
+}
