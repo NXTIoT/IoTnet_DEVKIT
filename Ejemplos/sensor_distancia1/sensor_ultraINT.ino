78eb82825c3162f1132658b26b2e4c70369149ab
NXTIoT <33075322+NXTIoT@users.noreply.github.com>
2018-05-15 19:28:09 -0500

    Update sensor_ultraINT.ino

--- Ejemplos/sensor_distancia1/sensor_ultraINT.ino
+++ Ejemplos/sensor_distancia1/sensor_ultraINT.ino
@@ -1,27 +1,26 @@
 //Programa de ejemplo utilizando el sensor ultrasonico HC-SR04
-//En este caso el dato en Entero (int) es transformado a su representacion en hexadecimal
+//En este caso el dato en entero es transformado a su 
+//representacion en hexadecimal en 1 byte
 #define trigPin 3
 #define echoPin 2
 const int boton=6;
-
+//*****************************************************
+String bufer;
+String bufer2="\n";
+//*************************
 void setup() 
 {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(boton, INPUT);
-  pinMode(7, OUTPUT);
+  pinMode(7, OUTPUT);   //enable modulo wisol
 }
 
 void leer_distancia()
 {
-  //AT$SF= comando para mandar la informacion por sigfox
-  //Maximo 12 bytes
-  String bufer="AT$SF=";
-  //*************************
-  String bufer2="\n";
   float duracion;
-  unsigned int distancia ;
+  int distancia ;
   digitalWrite(trigPin, LOW);        
   delayMicroseconds(2);              
   digitalWrite(trigPin, HIGH);       
@@ -31,19 +30,62 @@ void leer_distancia()
   distancia = duracion / 2 / 29.1;
   Serial.print("Distancia: ") ;
   Serial.println(distancia) ;
-  //Conversion de nuestro dato a bytes
-  byte* a1 = (byte*) &distancia;
+  //agregamos nuestro valor de la distancia al payload a enviar
+  add_int(distancia); //un entero menor a 256 ocupa un byte
+
+  //enviamos nuestro dato por Sigfox
+  send_message(bufer);
+}
+
+void loop() 
+{
+  if (digitalRead(boton)==LOW)
+  { 
+    //-----------------------------------------------------
+    //AT$SF= comando para mandar la informacion por sigfox
+    //Maximo 12 bytes
+    bufer="AT$SF=";
+    //-----------------------------------------------------
+    leer_distancia();
+    delay(1000);
+  } 
+}
+void add_float(float var1) //funcion para agregar flotantes al payload
+{
+  byte* a1 = (byte*) &var1;    //convertimos el dato a bytes
   String str1;
-  //agregamos al comando AT$SF nuestra informacion a enviar
-    str1=String(a1[0], HEX);
+  //agregamos al comando AT$SF= nuestra informacion a enviar
+  for(int i=0;i<4;i++)
+  {
+    str1=String(a1[i], HEX);    //convertimos el valor hex a string 
     if(str1.length()<2)
     {
-      bufer+=0+str1;
+      bufer+=0+str1;    //si no, se agrega un cero
     }
     else
     {
-      bufer+=str1;
-    } 
+      bufer+=str1;    //si esta completo, se copia tal cual
+    }
+  }
+}
+void add_int(int var2)    //funcion para agregar enteros al payload (hasta 255)
+{
+  byte* a2 = (byte*) &var2; //convertimos el dato a bytes
+  String str2;
+  str2=String(a2[0], HEX);  //convertimos el valor hex a string 
+  //verificamos si nuestro byte esta completo
+  if(str2.length()<2)
+  {
+    bufer+=0+str2;    //si no, se agrega un cero
+  }
+  else
+  {
+    bufer+=str2;     //si esta completo, se copia tal cual
+  }
+}
+void send_message(String payload)
+{
+  //agregamos el salto de linea "\n"
   bufer+=bufer2;
   //*******************
   //Habilitamos el modulo Sigfox
@@ -57,14 +99,4 @@ void leer_distancia()
   delay(3000);
   //deshabilitamos el modulo Sigfox
   digitalWrite(7, LOW);
-  delay(500);
-}
-
-void loop() 
-{
-  if (digitalRead(boton)==LOW)
-  { 
-    leer_distancia();
-    delay(1000);
-  } 
 }
