8a9296f301edb62cc0e681451ab09eef6beecc5e
NXTIoT <33075322+NXTIoT@users.noreply.github.com>
2018-11-30 09:50:26 -0600

    Update prub_dev.ino

--- Ejemplos/prub_dev.ino
+++ Ejemplos/prub_dev.ino
@@ -1,129 +1,101 @@
-/***************************************************************************
-  This is a library for the BME280 humidity, temperature & pressure sensor
-  Designed specifically to work with the Adafruit BME280 Breakout
-  ----> http://www.adafruit.com/products/2650
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface. The device's I2C address is either 0x76 or 0x77.
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
-
-unsigned long delayTime;
-String bufer;
-String bufer2="\n";
-
-void setup() {
-    Serial.begin(9600);
-    Serial.println(F("BME280 test"));
-    
-    pinMode(7, OUTPUT);
-
-    bool status;
-    
-    // default settings
-    // (you can also pass in a Wire library object like &Wire2)
-    status = bme.begin();  
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring!");
-        while (1);
-    }
-    
-    Serial.println("-- Default Test --");
-    delayTime = 300000;
-
-    Serial.println();
-}
-
-
-void loop() { 
-    bufer="AT$SF=";
-    printValues();
-    delay(delayTime);
-}
-
-
-void printValues() {
-    Serial.print("Temperature = ");
-    int temp=bme.readTemperature();
-    Serial.print(temp);
-    Serial.println(" *C");
-    add_int(temp);
-    Serial.print("Pressure = ");
-    float presion=bme.readPressure() / 100.0F;
-    Serial.print(presion);
-    Serial.println(" hPa");
-    add_float(presion);
-    Serial.print("Approx. Altitude = ");
-    float altura=bme.readAltitude(SEALEVELPRESSURE_HPA);
-    Serial.print(altura);
-    Serial.println(" m");
-    add_float(altura);
-    Serial.print("Humidity = ");
-    int hum=bme.readHumidity();
-    Serial.print(hum);
-    Serial.println(" %");
-    add_int(hum);
-    Serial.println();
-    send_message(bufer);
-}
-
-void add_float(float var1)
+//Programa de ejemplo para enviar informacion por medio de Sigfox
+//representacion en hexadecimal de un entero (1 byte) o de un 1 flotante a 32 bits en little-endian
+//Al presionar el boton azul se envia un mensaje
+const int boton=6;
+const int enable=7;
+int randNumber;
+//*****************************************************
+String bufer; //variable donde guardaremos nuestro payload
+String bufer2="\n";   //agregamos un salto de linea al final de nuestro payload
+//*****************************************************
+void setup() 
 {
-  byte* a1 = (byte*) &var1;
+  Serial.begin(9600);
+  pinMode(boton, INPUT);
+  pinMode(enable, OUTPUT);   //enable modulo wisol
+}
+
+void loop() 
+{
+  if (digitalRead(boton)==LOW)
+  {
+    function();
+    delay(1000);
+  } 
+}
+void function()
+{
+  //-----------------------------------------------------
+  //tu codigo
+  float latitud= 19.199934;
+  float longitud= -96.159825;
+  int bat =85;
+  randomSeed(analogRead(A0));    // genera una semilla para aleatorio a partir de la función millis()
+  randNumber = random(2000,2500);
+  float temp=randNumber/100.0;
+  randomSeed(analogRead(A1));    // genera una semilla para aleatorio a partir de la función millis()
+  randNumber = random(3000,3200);
+  float hum=randNumber/100.0;
+
+  
+  /*-----------------------------------------------------
+  //inicializamos el payload cargando el comando para enviar
+  //AT$SF= comando para mandar la informacion por sigfox Maximo 12 bytes*/
+  bufer="AT$SF=";
+  
+  /**************************************************
+  agregamos nuestra variable al payload a enviar*/
+  add_float(latitud);
+  add_float(longitud);
+  add_int(temp);
+  add_int(bat);
+  //add_int(variable_int); //un entero ocupa 1 byte
+  //add_float(variable_float); //un flotante ocupa 4 bytes
+  
+
+  //enviamos nuestro dato por Sigfox
+  send_message(bufer);
+}
+void add_float(float var1) //funcion para agregar flotantes al payload en formato little-endian
+{
+  byte* a1 = (byte*) &var1;    //convertimos el dato a bytes
   String str1;
   //agregamos al comando AT$SF= nuestra informacion a enviar
   for(int i=0;i<4;i++)
   {
-    str1=String(a1[i], HEX);
+    str1=String(a1[i], HEX);    //convertimos el valor hex a string 
     if(str1.length()<2)
     {
-      bufer+=0+str1;
+      bufer+=0+str1;    //si no, se agrega un cero
     }
     else
     {
-      bufer+=str1;
+      bufer+=str1;    //si esta completo, se copia tal cual
     }
   }
 }
-void add_int(int var2)
+void add_int(int var2)    //funcion para agregar enteros al payload (hasta 255)
 {
-  byte* a2 = (byte*) &var2;
+  byte* a2 = (byte*) &var2; //convertimos el dato a bytes
   String str2;
-  str2=String(a2[0], HEX);
+  str2=String(a2[0], HEX);  //convertimos el valor hex a string 
+  //verificamos si nuestro byte esta completo
   if(str2.length()<2)
   {
-    bufer+=0+str2;
+    bufer+=0+str2;    //si no, se agrega un cero
   }
   else
   {
-    bufer+=str2;
+    bufer+=str2;     //si esta completo, se copia tal cual
   }
 }
 void send_message(String payload)
 {
+  //agregamos el salto de linea "\n"
   bufer+=bufer2;
   //*******************
   //Habilitamos el modulo Sigfox
-  digitalWrite(7, HIGH);
+  digitalWrite(enable, HIGH);
   delay(1000);
   //Reset del canal para asegurar que manda en la frecuencia correcta
   Serial.print("AT$RC\n"); 
@@ -132,6 +104,5 @@ void send_message(String payload)
   Serial.print(bufer);
   delay(3000);
   //deshabilitamos el modulo Sigfox
-  digitalWrite(7, LOW);
-  //delay(500);
+  digitalWrite(enable, LOW);
 }
